0292059252398a41f8b66bdefb7aed84c69e5086
Askhat Tokombayev <askhat1990@yahoo.com>
2017-11-03 18:08:46 -0500

    Added Dog class

---
+++ Dog.py
@@ -0,0 +1,7 @@
+from collections import OrderedDict
+favorite_languages = OrderedDict()
+favorite_languages['AT'] = ['Python', 'English']
+favorite_languages['MT'] = ['C', 'Spanish']
+
+print(favorite_languages)
+
